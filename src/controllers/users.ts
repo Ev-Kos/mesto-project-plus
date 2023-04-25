0d41fb72de39e4cd5feda57d07af6ae71508df45
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-04-25 10:39:41 +0300

    added controllers and routes for user

---
+++ src/controllers/users.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import User from '../models/user';
+
+export const getUsers = (req: Request, res: Response, next: NextFunction) => {
+    return User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => next(err));
+}
+
+export const getUserById = (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  User.find({ _id: id })
+    .then((users) => res.status(200).send(users))
+    .catch((err) => next(err));
+};
+
+export const createUser = (req: Request, res: Response, next: NextFunction) => {
+  const { name, about, avatar } = req.body;
+
+  return User.create({ name, about, avatar })
+    .then((user) => res.status(200).send(user))
+    .catch((err) => next(err));
+}
---
+++ src/routes/users.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import {
+  getUsers,
+  getUserById,
+  createUser
+} from '../controllers/users';
+
+const userRouter = Router();
+
+userRouter.get('/', getUsers);
+userRouter.get('/:id', getUserById);
+userRouter.post('/', createUser);
+
+export default userRouter;
